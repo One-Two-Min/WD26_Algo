fee007413b020b6c9a5fb1f79c0890d656735682
WD26 <onevschoi@gmail.com>
2021-10-15 18:47:29 +0900

    [2021-10-15] LeetCode 207. Course Schedule

---
+++ LeetCode/207. Course Schedule/207. Course Schedule.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)] # 강의 수강 후 들을 수 있는 강의의 리스트
+        indegree = [0 for _ in range(numCourses)] # 강의의 선수 강의의 수를 담기 위한 리스트
+
+        # 강의 리스트와 선수 강의의 수 초기화
+        for (next, prev) in prerequisites:
+            graph[prev].append(next)
+            indegree[next] += 1
+        
+        q = deque()
+
+        # 선수 강의의 수가 0인 강의들을 큐에 담아준다.
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        # 큐를 돌면서 now 강의를 들었을 때 들을 수 있는 next 강의들의 선수 강의 수를 줄여준다.
+        # next 강의의 선수 강의 수가 0이 되면 이제 next 강의를 들을 수 있는 것이므로 큐에 새롭게 넣어준다.
+        while q:
+            now = q.popleft()
+
+            for next in graph[now]:
+                indegree[next] -= 1
+
+                if indegree[next] == 0:
+                    q.append(next)
+
+        # 위의 과정을 마쳤을 때 아직 선수 강의가 남아있는 강의가 있을 경우에는
+        # False를 리턴한다.
+        for course in indegree:
+            if course != 0:
+                return False
+        
+        return True
